4c0481635d561a5094c8c09d98aac6d60bb45f6a
Kazzkiq <claudiohw@gmail.com>
2017-05-20 22:36:37 -0300

    Lots of changes

--- brunch-config.js
+++ brunch-config.js
@@ -5,6 +5,10 @@ module.exports = {
     javascripts: {joinTo: 'app.js'},
     stylesheets: {joinTo: 'app.css'}
   },
+  plugins: {
+    babel: {presets: ['latest', 'react']},
+    postcss: {processors: [require('autoprefixer')]}
+  },
   modules: {
     autoRequire: { 'app.js': ['initialize'] }
   }
